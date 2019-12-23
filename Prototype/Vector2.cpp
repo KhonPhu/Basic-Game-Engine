a5bf574922f6005ad2b097a2418e16743b9dd70f
Khon Phu <khonphu@outlook.com>
2019-12-23 13:18:17 +1100

    new 1

--- Prototype/Vector2.cpp
+++ Prototype/Vector2.cpp
@@ -1,1 +1,78 @@
-#include "Vector2.h"
+﻿#include "Vector2.h"
+#include <SDL.h>
+
+Vector2::Vector2()
+{
+	x = 0;
+	y = 0;
+}
+
+Vector2::Vector2(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+Vector2::~Vector2()
+{
+}
+
+float Vector2::Length()
+{
+	return sqrtf(x * x + y * y);
+}
+
+Vector2 Vector2::operator+(const Vector2 & anotherV)
+{
+	return Vector2(this->x + anotherV.x, this->y + anotherV.y);
+}
+
+Vector2 Vector2::operator+=(const Vector2 & anotherV)
+{
+	this->x += anotherV.x;
+	this->y += anotherV.y;
+	return* this;
+}
+
+Vector2 Vector2::operator-(const Vector2 & anotherV)
+{
+	return Vector2(this->x - anotherV.x, this->y - anotherV.y);
+}
+
+Vector2 Vector2::operator-=(const Vector2& anotherV)
+{
+	this->x -= anotherV.x;
+	this->y -= anotherV.y;
+	return* this;
+}
+
+Vector2 Vector2::operator*(const float &scalar)
+{
+	return Vector2(this->x * scalar, this->y * scalar);
+}
+Vector2 Vector2::operator*=(const float &scalar)
+{
+	this->x *= scalar;
+	this->y *= scalar;
+	return*this;
+}
+
+void Vector2::Normalize()
+{
+	float mag = sqrtf(x * x + y * y);
+	this->x /= mag;
+	this->y /= mag;
+}
+
+Vector2 Vector2::Normalized()
+{
+	Vector2 result = Vector2(0,0);
+	float mag = sqrtf(x * x + y * y);
+	result.x = this->x / mag;
+	result.y = this->y / mag;
+
+	return result;
+}
+
+
+
--- Prototype/Vector2.h
+++ Prototype/Vector2.h
@@ -1,5 +1,36 @@
 #pragma once
+#include <math.h>
+#include <vector>
+
 class Vector2
 {
+public:
+	int x;
+	int y;
+
+public:
+	Vector2();
+	Vector2(int x, int y);
+	~Vector2();
+
+	float Length();
+
+	Vector2 operator+(const Vector2& anotherV);
+
+	Vector2 operator+=(const Vector2& anotherV);
+
+	Vector2 operator-(const Vector2& anotherV);
+
+	Vector2 operator-=(const Vector2& anotherV);
+
+	Vector2 operator*(const float& scalar);
+
+	Vector2 operator*=(const float& scalar);
+
+	void Normalize();
+
+	Vector2 Normalized();
+
+
 };
 
